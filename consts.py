f682703b06f0ebb6289cbec4128226b9d43dfe92
screen <wolf1.noam@gmail.com>
2025-08-24 15:12:00 +0300

    CONSTS

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 import pygame
-WINDOW_WIDTH=400
-WINDOW_HEIGHT=200
+WINDOW_WIDTH=1000
+WINDOW_HEIGHT=800
 SOLDIER_PNG=pygame.image.load('soldier.png')
 GRASS_PNG=pygame.image.load('grass.png')
 FLAG_PNG=pygame.image.load('flag.png')
