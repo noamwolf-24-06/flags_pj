3d3e849b45cd58978927ec4c9eaf4e66774a44f4
screen <wolf1.noam@gmail.com>
2025-08-24 12:54:09 +0300

    add consts

---
+++ consts.py
@@ -0,0 +1,7 @@
+import pygame
+WINDOW_WIDTH=400
+WINDOW_HEIGHT=200
+SOLDIER_PNG=pygame.image.load('soldier.png')
+GRASS_PNG=pygame.image.load('grass.png')
+FLAG_PNG=pygame.image.load('flag.png')
+BACKGROUND_COLOR=(16,130,16)
