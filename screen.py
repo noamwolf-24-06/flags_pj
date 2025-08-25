7bef834636d5fa746f2ec266a620e4fb81f33f00
dina <dina.grabois@gmail.com>
2025-08-25 11:27:33 +0300

    screen and soldier

--- screen.py
+++ screen.py
@@ -3,6 +3,7 @@ import pygame
 import random
 import consts
 import game_field
+import soldier
 
 background_colour = consts.BACKGROUND_COLOR
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
@@ -23,6 +24,9 @@ def draw_a_bush(x, y):
     GRASS = pygame.transform.scale(consts.GRASS_PNG, (50, 50))
     screen.blit(GRASS, (x, y))
 
+def make_soldier():
+    SOLDIER = pygame.transform.scale(consts.SOLDIER_PNG, (80,80))
+    return screen.screen.blit(SOLDIER,consts.START_PLAYER)
 
 def draw_grid():
     for x in range(consts.TILE_SIZE, consts.WINDOW_WIDTH, consts.TILE_SIZE):
@@ -34,20 +38,23 @@ def draw_grid():
 
 
 def draw_mine():
-    MINE = pygame.transform.scale(consts.MINE_PNG, (20, 20))
+    MINE = pygame.transform.scale(consts.MINE_PNG, (60, 20))
     for i in range(consts.NUM_OF_ROWS):
         for j in range(consts.NUM_OF_COLS):
             if game_field.matrix_grid[i][j] == "M":
-                coordinates = ((i + 1) * consts.TILE_SIZE,
-                               ((j + 1) * consts.TILE_SIZE))
+                coordinates = (j * (consts.TILE_SIZE)), (i * (consts.TILE_SIZE))
                 screen.blit(MINE, coordinates)
 
-
 spawn_bushes()
+draw_grid()
+draw_mine()
+soldier.make_soldier()
 
 running = True
-
+part_one = True
 while running:
+    # if part_one:
+    #     soldier.make_soldier()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- soldier.py
+++
@@ -1,4 +0,0 @@
-import pygame
-import consts
-
-def make_soldier():
