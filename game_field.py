bc10fe73962845ebf06ee0cecfcfe0845d1a4205
screen <wolf1.noam@gmail.com>
2025-08-25 12:00:43 +0300

    game field

--- game_field.py
+++ game_field.py
@@ -12,14 +12,14 @@ matrix_grid=create_matrix()
 def flag_loc(matrix_grid):
     for i in range(4,0,-1):
         for j in range(3,0,-1):
-            matrix_grid[consts.NUM_OF_ROWS-j-2][consts.NUM_OF_COLS-i]="F"
+            matrix_grid[consts.NUM_OF_ROWS-j-1][consts.NUM_OF_COLS-i]="F"
     return matrix_grid
 matrix_grid=flag_loc(matrix_grid)
 def mine_in_matrix(matrix_grid):
     for i in range (20):
         random_row = random.randrange(consts.NUM_OF_ROWS)
         random_col = random.randrange(consts.NUM_OF_COLS)
-        if matrix_grid[random_row][random_col]=="F" or random_col>47:
+        if matrix_grid[random_row][random_col]=="F" or random_col>47 or (random_col>46 and random_row==24):
             random_row = random.randrange(consts.NUM_OF_ROWS)
             random_col = random.randrange(consts.NUM_OF_COLS)
         for row in range(len(matrix_grid)):
