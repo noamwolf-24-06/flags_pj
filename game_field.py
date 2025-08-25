fd64faca0b1ae228256ffd25d0180b7f2243c2c1
screen <wolf1.noam@gmail.com>
2025-08-25 11:52:30 +0300

    game field

--- game_field.py
+++ game_field.py
@@ -9,10 +9,19 @@ def create_matrix():
         game_grid.append(row)
     return game_grid
 matrix_grid=create_matrix()
+def flag_loc(matrix_grid):
+    for i in range(4,0,-1):
+        for j in range(3,0,-1):
+            matrix_grid[consts.NUM_OF_ROWS-j-2][consts.NUM_OF_COLS-i]="F"
+    return matrix_grid
+matrix_grid=flag_loc(matrix_grid)
 def mine_in_matrix(matrix_grid):
     for i in range (20):
         random_row = random.randrange(consts.NUM_OF_ROWS)
         random_col = random.randrange(consts.NUM_OF_COLS)
+        if matrix_grid[random_row][random_col]=="F" or random_col>47:
+            random_row = random.randrange(consts.NUM_OF_ROWS)
+            random_col = random.randrange(consts.NUM_OF_COLS)
         for row in range(len(matrix_grid)):
             for col in range(len(matrix_grid[row])):
                 if row==random_row and col == random_col:
@@ -22,6 +31,6 @@ matrix_grid=mine_in_matrix(matrix_grid)
 def print_matrix(matrix_grid):
     for m in matrix_grid:
         for n in m:
-            print(n,end=" ")
+            print(n, end=" ")
         print()
 print_matrix(matrix_grid)
